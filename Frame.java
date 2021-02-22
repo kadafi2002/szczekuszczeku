183271c6cf606baab00dc3db5ed47574e59406da
kadafi2002 <kadafi2002@gmail.com>
2021-02-22 16:46:03 +0100

    trójkąty i kwadraty

--- Frame.java
+++ Frame.java
@@ -7,14 +7,14 @@ private static final long serialVersionUID = 1L;
 private Component add;
 public Frame(){
 super("kółko i krzyżyk");
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-setSize(300,400);
-setLocation(700,100);
-setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300,400);
+        setLocation(700,100);
+        setVisible(true);
 
 JPanel buttonsPanel = new Buttons();
 add = add(buttonsPanel);
 }
 
 
-}
+}   
